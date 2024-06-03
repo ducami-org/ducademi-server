070be0b64d234650a4bac52fb6a3f64d5f2a9ab9
김호준 <hojoon4868@gmail.com>
2024-06-03 20:32:49 +0900

    feat :: 기본 설정

---
+++ src/main/java/org/ducami/ducademi/server/DucademiServerApplication.java
@@ -0,0 +1,13 @@
+package org.ducami.ducademi.server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DucademiServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DucademiServerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/ducami/ducademi/server/DucademiServerApplicationTests.java
@@ -0,0 +1,13 @@
+package org.ducami.ducademi.server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DucademiServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
